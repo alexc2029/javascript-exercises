ffe57772ea58099f56919808258921450860730e
alexc2029 <alex2akec@gmail.com>
2025-04-03 15:16:18 +0300

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(intervalStart, intervalEnd) {
+    let sum=0;
+    if(intervalStart>intervalEnd)
+    {
+        let aux=intervalStart;
+        intervalStart=intervalEnd;
+        intervalEnd=aux;
+    }
+    if(intervalStart < 0 || intervalEnd < 0)
+        return "ERROR";
+    if(!(Number.isInteger(intervalStart)) || !(Number.isInteger(intervalEnd)))
+        return "ERROR";
+    for(let i=intervalStart;i<=intervalEnd;i++)
+    {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
