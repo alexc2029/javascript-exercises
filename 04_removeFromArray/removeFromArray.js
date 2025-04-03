671aee543025d5fde83cfb24108662f4248fb612
alexc2029 <alex2akec@gmail.com>
2025-04-03 15:04:36 +0300

    Finish fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for(let i=0;i<array.length;i++)
+    {
+        for(const val of values)
+            if(array[i]==val && typeof array[i] == typeof val)
+            {
+                array.splice(i,1);
+                i--;
+            }
+                
+    }
+    return array;
 };
 
 // Do not edit below this line
