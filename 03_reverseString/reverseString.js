7183b3ad76d891ab479520858ca91b573617fe9d
alexc2029 <alex2akec@gmail.com>
2025-04-03 14:40:33 +0300

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arrayString = string.split("");
+    for(let i=0;i<(arrayString.length-1)/2;i++)
+    {
+        let aux=arrayString[i];
+        arrayString[i]=arrayString[arrayString.length-i-1];
+        arrayString[arrayString.length-i-1]=aux;
+    }
+    let reversedString=arrayString.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
