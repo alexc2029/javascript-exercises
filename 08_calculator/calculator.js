fc62e6295458fcf13d1fcb5557bb94912da40594
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-24 17:15:23 +0300

    Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	/*let sum = 0;
+    for (let i = 0; i < arr.length; i++) sum += arr[i];
+    return sum;
+    */
+	return arr.reduce((result, currentValue) => result + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	/*let result = 1;
+	for (let i = 0; i < arr.length; i++) result *= arr[i];
+	return result;
+  */
+	return arr.reduce((result, currentValue) => result * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+	let fact = 1;
+	for (let i = 2; i <= a; i++) fact *= i;
+	return fact;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
