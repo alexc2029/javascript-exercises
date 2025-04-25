0f9f41f6b4c903f9bf82054ef27853347cad8f0a
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-25 11:56:35 +0300

    Finish exercise. Check solution for alternative.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+	people.sort((a, b) => {
+		const date = new Date();
+		let year = date.getFullYear();
+		if (!a.yearOfDeath) a.yearOfDeath = year;
+		if (!b.yearOfDeath) b.yearOfDeath = year;
+		if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth)
+			return -1;
+		else return 1;
+	});
+	return people[0];
 };
 
 // Do not edit below this line
