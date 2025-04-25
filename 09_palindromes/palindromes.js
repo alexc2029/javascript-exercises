aac688f72ed6ecab335b7238904e557b001a5f5d
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-25 11:18:14 +0300

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.replace(/[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "").toLowerCase();
+	const reversed = str.split("").reverse().join("");
+	return str == reversed;
 };
 
 // Do not edit below this line
