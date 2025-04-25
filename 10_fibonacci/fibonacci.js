95b9bc57ac73e1f024927d9a100a194dfa74657e
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-25 11:38:06 +0300

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (pos) {
+	//pos = Number(pos);
+	if (pos == 0) return 0;
+	else if (pos < 0) return "OOPS";
+	let a = 1,
+		b = 1,
+		count = 2,
+		c;
+	while (count < pos) {
+		c = a + b;
+		a = b;
+		b = c;
+		count++;
+	}
+	return b;
 };
 
 // Do not edit below this line
